13f896795b2e611b3f67475b8d3ff4d98f183456
fabianriewe <fabianriewe00@gmail.com>
2019-12-20 13:04:59 +0100

    chore: minor proc

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <div>{{reciept}}</div>
+    <!-- <div>
+      <h1>Wallet</h1>
+      <div v-for="(account, index) in wallet" :key="index">{{account.address}}</div>
+    </div>-->
+    <div>Balance: {{ balance }}</div>
   </div>
 </template>
 
@@ -315,62 +320,68 @@ const contractAbi = [
   }
 ];
 const endpoint = "http://127.0.0.1:9545";
-const mnemonic =
-  "again indicate sadness search update gossip bitter truth empower dolphin caught club";
+//const mnemonic =
+//"again indicate sadness search update gossip bitter truth empower dolphin caught club";
 import Web3 from "web3";
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-var Accounts = require("web3-eth-accounts");
-var accounts = new Accounts(endpoint);
+//const HDWalletProvider = require("@truffle/hdwallet-provider");
+//var Accounts = require("web3-eth-accounts");
+//var accounts = new Accounts(endpoint);
 
 export default {
   name: "app",
   data() {
     return {
-      web3: new Web3(new HDWalletProvider(mnemonic, endpoint)),
+      web3: null,
       account: null,
       contractInstance: null,
       isLoading: false,
       reciept: null,
-      wallet: undefined
+      wallet: undefined,
+      balance: -1
     };
   },
   methods: {
-    balanceOf(address) {
-      this.isLoading = true;
-      this.contractInstance.methods
+    async balanceOf(address) {
+      console.log(address);
+      let balance = await this.contractInstance.methods
         .balanceOf(address)
-        .send({
-          from: this.account,
-          value: 0
-        })
-        .then(receipt => {
-          this.reciept = receipt;
-          this.isLoading = false;
-        })
-        .catch(err => {
-          console.log(err, "err");
-          this.isLoading = false;
-        });
+        .call();
+      return balance;
+    }
+  },
+  watch: {
+    async account() {
+      if (this.account) {
+        this.balance = await this.balanceOf(this.account);
+      }
     }
   },
   mounted() {
+    this.web3 = new Web3(endpoint);
     this.contractInstance = new this.web3.eth.Contract(
       contractAbi,
       contractAddress
     );
-    console.log(this.contractInstance);
-    this.wallet = this.web3.eth.accounts.wallet.load("Fabi1103!");
-    if (this.wallet.length <= 0) {
-      let newWallet = this.web3.eth.accounts.wallet.create(1);
-      console.log("NEUES WALLET ERSTELLT!");
-      console.log(newWallet);
-      this.web3.eth.accounts.wallet.save("Fabi1103!");
-      console.log("WALLET gespeichert");
-    } else {
-      console.log(this.wallet);
-      console.log("WALLET wurde geladen");
-    }
-    console.log(accounts);
+    this.web3.eth.getAccounts().then(accounts => {
+      console.log(accounts);
+      this.account = accounts[1];
+    });
+    // console.log(this.contractInstance);
+    // this.wallet = this.web3.eth.accounts.wallet.load("Fabi1103!");
+    // if (this.wallet.length <= 0) {
+    //   let newWallet = this.web3.eth.accounts.wallet.create(2);
+    //   console.log("NEUES WALLET ERSTELLT!");
+    //   console.log(newWallet);
+    //   this.web3.eth.accounts.wallet.save("Fabi1103!");
+    //   console.log("WALLET gespeichert");
+    // } else {
+    //   console.log(this.wallet);
+    //   console.log("WALLET wurde geladen");
+    // }
+    // console.log(this.wallet[0]);
+    // let accounts1 = await this.web3.eth.getAccounts();
+    // console.log(accounts1);
+    // console.log(accounts);
   }
 };
 </script>
