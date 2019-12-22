8ae09d4ebb3d3c019d9fa76f7290467a6b3ec84f
fabianriewe <fabianriewe00@gmail.com>
2019-12-22 22:37:14 +0100

    feat: added mockdata

--- src/components/offer.vue
+++ src/components/offer.vue
@@ -1,8 +1,8 @@
 <template>
   <v-card class="mx-auto" max-width="344">
-    <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+    <v-img :src="offers[id].image" height="200px"></v-img>
 
-    <v-card-title>Angebot {{id}}</v-card-title>
+    <v-card-title>{{ offers[id].name }}</v-card-title>
 
     <v-card-subtitle>{{balance}} Tokens</v-card-subtitle>
   </v-card>
@@ -13,7 +13,25 @@ export default {
   name: "app",
   props: ["balance", "id"],
   data() {
-    return {};
+    return {
+      offers: [
+        {
+          name: "Audi R8",
+          image:
+            "https://i.ebayimg.com/00/s/NDgwWDY0MA==/z/O-4AAOSwIvdd37Bd/$_20.jpg"
+        },
+        {
+          name: "Porsche 911",
+          image:
+            "https://i.ebayimg.com/00/s/MTIwMFgxNjAw/z/zAEAAOSw7xZd1Sl4/$_20.jpg"
+        },
+        {
+          name: "Mercedes-Benz SL 380",
+          image:
+            "https://i.ebayimg.com/00/s/MTIwMFgxNjAw/z/kukAAOSw-c9d~TYe/$_20.jpg"
+        }
+      ]
+    };
   }
 };
 </script>
